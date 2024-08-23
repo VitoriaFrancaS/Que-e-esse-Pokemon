ab10933aa3511c73b27638c9aa61fbd8d34e995f
vitoria.franca <vitoria.franca@performait.com>
2024-08-23 10:46:42 -0300

    first commit

---
+++ Pokemon.cs
@@ -0,0 +1,24 @@
+﻿namespace QuemEEssePokemon;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<TypeInfo> Types { get; set; }
+    public Sprites Sprites { get; set; }
+}
+
+public class TypeInfo
+{
+    public TypeDetalhe Type { get; set; }
+}
+
+public class TypeDetalhe
+{
+    public string Name { get; set; }
+}
+
+public class Sprites
+{
+    public string Front_default { get; set; }
+}
